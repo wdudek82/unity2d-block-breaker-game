05d5ad71bd8c96ac44288bd4f86a69096e76aac6
Wojciech Dudek <wdudek82@gmail.com>
2019-01-05 22:45:11 +0100

    Added, and prefabed, GameStatus obj that allows to modify game speed.

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    [Range(0.1f, 10f)] [SerializeField] private float gameSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+}
